4162259dc1999b7114c263d711de4657a8d3ee50
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2023-04-14 23:59:55 +0900

    参照を修正

--- routes/admin.php
+++ routes/admin.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\Admin\AuthenticatedSessionController;
-use App\Http\Controllers\Admin\ConfirmablePasswordController;
-use App\Http\Controllers\Admin\EmailVerificationNotificationController;
-use App\Http\Controllers\Admin\EmailVerificationPromptController;
-use App\Http\Controllers\Admin\NewPasswordController;
-use App\Http\Controllers\Admin\PasswordResetLinkController;
-use App\Http\Controllers\Admin\RegisteredUserController;
-use App\Http\Controllers\Admin\VerifyEmailController;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Admin\Auth\NewPasswordController;
+use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Admin\Auth\RegisteredUserController;
+use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 
