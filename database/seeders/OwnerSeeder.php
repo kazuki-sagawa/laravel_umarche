63a9c276fea81f98de511a2c6a75695daab736a4
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2023-04-18 23:09:58 +0900

    pagination

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -38,6 +38,27 @@ class OwnerSeeder extends Seeder
                 'created_at' => '2021/01/01 11:11:11'
 
             ],
+            [
+                'name' => 'test4',
+                'email' => 'tes4t@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2021/01/01 11:11:11'
+
+            ],
+            [
+                'name' => 'test5',
+                'email' => 'tes5t@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2021/01/01 11:11:11'
+
+            ],
+            [
+                'name' => 'test6',
+                'email' => 'tes6t@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2021/01/01 11:11:11'
+
+            ],
         ]);
 
     }
