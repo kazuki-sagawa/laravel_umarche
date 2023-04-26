fe47c5e23adcfc69bed781707af6010ef61877c6
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2023-04-26 23:46:46 +0900

    サービスへの切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -56,15 +57,7 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            // リサイズしないパターン
-            // Storage::putFile('public/shops', $imageFile);
-
-            $fileName = uniqid(rand().'_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName . '.' . $extension;
-            $resizedImage = InterventionImage::make($imageFile)
-                ->resize(1920, 1080)->encode();
-            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
         return redirect()->route('owner.shops.index');
     }
---
+++ app/Services/ImageService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+
+
+class ImageService
+{
+    public static function upload($imageFile, $folderName)
+    {
+        // リサイズしないパターン
+        // Storage::putFile('public/shops', $imageFile);
+
+        $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName . '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)
+                ->resize(1920, 1080)->encode();
+
+            Storage::put('public/' . $fileName . '/' . $fileNameToStore, $resizedImage);
+        return $fileNameToStore;
+    }
+}
